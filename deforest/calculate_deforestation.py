69d3a691bcdfa40c41840204b56b40eab6453cbf
sambowers <sam.bowers@ed.ac.uk>
2017-12-20 19:44:23 +0000

    Now both S1/S2 working, though S2 not very well

--- deforest/calculate_deforestation.py
+++ deforest/calculate_deforestation.py
@@ -9,42 +9,18 @@ import csv
 
 import pdb
 
-def world2Pixel(geoMatrix, x, y):
-    """
-    Uses a gdal geomatrix (gdal.GetGeoTransform()) to calculate
-    the pixel location of a geospatial coordinate
-    """
-    ulX = geoMatrix[0]
-    ulY = geoMatrix[3]
-    xDist = geoMatrix[1]
-    yDist = geoMatrix[5]
-    rtnX = geoMatrix[2]
-    rtnY = geoMatrix[4]
-    pixel = int((x - ulX) / xDist)
-    line = int((ulY - y) / xDist)
-    return (pixel, line)
 
-def getPixel(data_files, mask_files, E, N):
+
+def combineObservations(probability, mask):
     '''
+    For cases where there are more than one observations for a given day, here their forest probabilities are combined.
     '''
     
-    pixel, line = world2Pixel(gdal.Open(data_files[0],0).GetGeoTransform(),E,N)
-        
-    data_out = []
-    date = []
+    probability = np.ma.array(probability, mask = mask)
     
-    for data_file, mask_file in zip(data_files,mask_files):
-        
-        print 'Doing %s'%data_file.split('/')[-1]
-        mask = gdal.Open(mask_file,0).ReadAsArray()[line,pixel]
-        
-        if mask != 1:
-            print 'Got one!'
-            data_out.append(gdal.Open(data_file,0).ReadAsArray()[line,pixel])
-            datestring = data_file.split('/')[-1].split('.tif')[0].split('_')[3]
-            date.append(dt.date(int(datestring[:4]),int(datestring[4:6]),int(datestring[6:])))
-    
-    return data_out, date
+    probability_combined = np.prod(probability, axis = 2) / (np.prod(probability, axis = 2) + np.prod(1 - probability, axis = 2))
+       
+    return probability_combined.data, probability_combined.mask
 
 
 def bayesUpdate(prior, likelihood):
@@ -56,27 +32,6 @@ def bayesUpdate(prior, likelihood):
     return posterior
 
 
-def combineObservations(probability, mask):
-    '''
-    For cases where there are more than one observations for a given day, here their forest probabilities can be combined.
-    '''
-        
-    # Calculate number of observations
-    nobs = np.sum(mask, axis=2)
-    
-    probability_out = np.zeros_like(nobs, dtype = np.float)
-    
-    probability_out[nobs > 1] = np.array([np.prod(probability[nobs > 1, :], axis = 1) / (np.prod(probability[nobs > 1, :], axis = 1) + np.prod(1 - probability[nobs > 1, :], axis = 1))])[0,:]
-    
-    for observation in range(probability.shape[2]):
-        
-        probability_out[np.logical_and(mask[:,:,observation], nobs == 1)] =  probability[:,:,observation][np.logical_and(mask[:,:,observation], nobs == 1)]
-    
-        
-    # Update mask where there's at least one valid measurement
-    mask_out = nobs >= 1
-    
-    return probability_out, mask_out
 
 
 
@@ -87,130 +42,79 @@ def combineObservations(probability, mask):
 # Load each image in turn, and calculate the probability of forest (from a start point of everything being forest)
 
 
-data_files = glob.glob('/exports/eddie/scratch/sbowers3/chimanimani/L3_files/chimanimaniGlobal*_data.tif')
-data_files.sort(key = lambda x: x.split('_')[4])
+data_files = glob.glob('/exports/csce/datastore/geos/groups/SMFM/chimanimani/L3_data/chimanimaniGlobal_*.tif')
+data_files.sort()
 data_files = np.array(data_files)
 
-mask_files = np.array([i[:-8] + 'mask.tif' for i in data_files])
+datestrings = ['_'.join(x.split('/')[-1].split('_')[1:3]) for x in data_files]
+datetimes = np.array([dt.datetime.strptime(date,'%Y%m%d_%H%M%S') for date in datestrings],dtype='datetime64[s]')
+dates = datetimes.astype('datetime64[D]')
 
-datestrings = [x.split('/')[-1].split('_')[3] for x in data_files]
-dates = np.array([dt.date(int(x[:4]), int(x[4:6]), int(x[6:])) for x in datestrings])
+image_type = np.array(['_'.join(x.split('/')[-1].split('.')[-2].split('_')[3:5]) for x in data_files])
 
-sensors = np.array([x.split('/')[-1].split('_')[1] for x in data_files])
-pols = np.array([x.split('/')[-1].split('_')[2] for x in data_files])
-
-# Get selection that removes VV S1 files where a VH file exists
-'''
-sel = np.logical_and(np.in1d(dates,np.intersect1d(dates[pols == 'VH'],dates[pols == 'VV'])), pols == 'VV') == False
-
-data_files = data_files[sel]
-mask_files = mask_files[sel]
-dates = dates[sel]
-sensors = sensors[sel]
-pols = pols[sel]
-'''
 
 # Initialise arrays
-deforestation = np.zeros_like(gdal.Open(data_files[0]).ReadAsArray(), dtype=np.bool)#[2500:2600,2500:2600]
+ds = gdal.Open(data_files[0])
+
+YSize = ds.RasterYSize
+XSize = ds.RasterXSize
+
+deforestation = np.zeros((YSize, XSize), dtype=np.bool)
 previous_flag = np.zeros_like(deforestation, dtype=np.bool)
 false_alarms = np.zeros_like(deforestation, dtype=np.bool)
 PNF_last = np.zeros_like(deforestation, dtype = np.float) + 0.5 # Initialise to 0.5 probability of no forest
 deforestation_date = np.zeros_like(deforestation, dtype='datetime64[D]')
 pchange = np.zeros_like(deforestation, dtype=np.float)
 
-    
-# Get unique dates
-for date in sorted(np.unique(dates)):
-    
-    sensor = sensors[dates == date]
-    pol = pols[dates == date]    
-    
-    '''
-    F_mean = np.zeros_like(sensors[dates == date], dtype = np.float64)
-    F_sd = np.zeros_like(F_mean)
-    NF_mean = np.zeros_like(F_mean)
-    NF_sd = np.zeros_like(F_mean)
-    
-    # Sentinel-1 (VV)
-    F_mean[np.logical_and(sensor=='S1',pol=='VV')] = -0.91 #-1.19#-0.81#-7.
-    F_sd[np.logical_and(sensor=='S1',pol=='VV')] = 1.48 #1.8#0.94#0.75
-    NF_mean[np.logical_and(sensor=='S1',pol=='VV')] = -2.38 #-4.7#-4.38#-11.5
-    NF_sd[np.logical_and(sensor=='S1',pol=='VV')] = 1.84 #1.6#1.17#1
 
-    # Sentinel-1 (VH)
-    F_mean[np.logical_and(sensor=='S1',pol=='VH')] = -0.66
-    F_sd[np.logical_and(sensor=='S1',pol=='VH')] = 1.38
-    NF_mean[np.logical_and(sensor=='S1',pol=='VH')] = -2.51
-    NF_sd[np.logical_and(sensor=='S1',pol=='VH')] = 2.09
+# Run for each unique date
+for date in sorted(np.unique(dates)):
+       
+    unique_datetimes = np.unique(datetimes[dates == date])
+    unique_images = np.unique(image_type[dates == date])
     
-    # Sentinel-2
-    F_mean[sensor=='S2'] = -0.097#-0.11#-0.1#0.85
-    F_sd[sensor=='S2'] = 0.17#0.1337#0.08#0.075
-    NF_mean[sensor=='S2'] = -0.3#-0.28#-0.34 #0.4
-    NF_sd[sensor=='S2'] = 0.17#0.168#0.14#0.125      
-    '''
+    #TODO find repeat data in Sentinel-2 imagery. This is tricky, as datetimes can be different yet imagery the same in tile overlap.
     
-    # Load files (axis 2 when > 1 observation at a given date)
-    ds = gdal.Open(data_files[dates == date][0])
-    data = np.zeros((ds.RasterYSize, ds.RasterXSize, (dates == date).sum()))
-    mask_ind = np.zeros_like(data, dtype = np.bool)
+    # One layer per unique image type. This should allow only one overpass per satellite per granule/track. Needs work to accomodate Sentinel-2 tiles
     
-    for n, (data_file, mask_file) in enumerate(zip(data_files[dates == date], mask_files[dates == date])):
-        print 'Loading %s'%data_file
-        data[:,:,n] = gdal.Open(data_file,0).ReadAsArray()#[2500:2600,2500:2600]
-        mask_ind[:,:,n] = gdal.Open(mask_file,0).ReadAsArray()#[2500:2600,2500:2600]
+    #NB: Sentinel-2 currently contributing too much deforestation. Re-calibrate?
+    
+    p_forest = np.zeros((YSize, XSize, unique_images.shape[0]), dtype = np.uint8) + 255
+        
+    for n, this_image_type in enumerate(unique_images):
+        
+        # Only select one data file per image type
+        for data_file in data_files[np.logical_and(dates == date, image_type == this_image_type)]:
+                   
+            print 'Loading %s'%data_file
+            data = gdal.Open(data_file,0).ReadAsArray()
+            
+            # Select areas that still have the nodata value
+            s = p_forest[:,:,n] == 255
+            
+            # Paste the new data into these locations
+            p_forest[:,:,n][s] = data[s]
+            
+        
+    # Change percent probability of forest to probability of non forest
+    mask = p_forest == 255
+    PNF = (100 - p_forest) / 100.
+    PNF[mask] = 0.
     
     # Remove length-1 axes
-    #data = np.squeeze(data)
-    #mask = np.squeeze(mask)
-    
-    # Get probability of an observation being forest/nonforest
-    #PF = scipy.stats.norm.pdf(data, F_mean, F_sd)
-    #PNF = scipy.stats.norm.pdf(data, NF_mean, NF_sd)
-    
-    # Set up arrays of unique observations
-    obs = np.unique(sensor).shape[0]
-    if np.sum(sensor=='S2') > 1:
-        obs += (np.sum(sensor=='S2') - 1)
-    PNF = np.zeros((data.shape[0], data.shape[1], obs))
-    mask = np.zeros_like(PNF, dtype = np.bool)
-    
-    layer = 0
-    
-    if ('VV' in pol) and ('VH' in pol):
-        PNF[:,:,layer:layer+1] = (0.316 * data[:,:,pol == 'VV']) + (0.462 * data[:,:,pol == 'VH']) + 1.205
-        mask[:,:,layer:layer+1] = np.logical_and(mask_ind[:,:,pol == 'VV'], mask_ind[:,:,pol == 'VH'])
-        layer += 1
-    
-    if ('VV' in pol) and ('VH' not in pol):
-        PNF[:,:,layer:layer+1] = (0.644 * data[:,:,pol == 'VV']) + 1.182
-        mask[:,:,layer:layer+1] = mask_ind[:,:,pol == 'VV']
-        layer += 1
-    
-    if 'S2' in sensor:
-        PNF[:,:,layer:layer+np.sum(sensor=='S2')] = (6.797 * data[:,:,sensor == 'S2']) + 0.759
-        mask[:,:,layer:layer+np.sum(sensor=='S2')] = mask_ind[:,:,sensor == 'S2']
-        layer+=np.sum(sensor=='S2')
-
     PNF = np.squeeze(PNF)
     mask = np.squeeze(mask)
         
-    # Change log odds to probability
-    PNF = 1 - (np.exp(PNF) / (1 + np.exp(PNF)))
-    
-    # Determine conditional probability of an observation being from NF (From Reiche et al. 2018)
-    #PNF[PNF < 1E-10000] = 0
-    #PNF[PNF > 0] = (PNF[PNF > 0] / (PF[PNF > 0] + PNF[PNF > 0]))
-    
-    ## Apply block weighting function fudge.
-    PNF[PNF < 0.01] = 0.01
-    PNF[PNF > 0.99] = 0.99
+    ## Apply block weighting/
+    PNF[PNF < 0.1] = 0.1
+    PNF[PNF > 0.9] = 0.9
     
     # If multiple observations from the same date exist, combine them (not performed where only one observation)
-    if layer > 1:#(dates == date).sum() > 1:
+    if unique_images.shape[0] > 1:
         
         PNF, mask = combineObservations(PNF, mask)
-        
+    
+    
     # Flag potential changes
     flag = PNF > 0.5
         
